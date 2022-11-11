03fee49e5a43ef1a07fdcc74ca6d709ac7484969
wrhenders <wrhenders@gmail.com>
2022-11-11 10:45:49 -0600

    Added Trailing Icon modifier and finished card view

---
+++ Scrumdinger/Models/TrailingIconLabelStyle.swift
@@ -0,0 +1,21 @@
+//
+//  TrailingIconLabelStyle.swift
+//  Scrumdinger
+//
+//  Created by Ryan Henderson on 11/11/22.
+//
+
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack{
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,20 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by Ryan Henderson on 11/11/22.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView()
+    }
+}
