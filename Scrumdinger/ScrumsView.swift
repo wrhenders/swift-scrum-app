3ea9fa5bf4b5cde8d9069cca3e2d32fd76cd7430
wrhenders <wrhenders@gmail.com>
2022-11-14 16:00:49 -0600

    Added async await calls to Store

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -9,8 +9,10 @@ import SwiftUI
 
 struct ScrumsView: View {
     @Binding var scrums: [DailyScrum]
+    @Environment(\.scenePhase) private var scenePhase
     @State private var isPresentingNewScrumView = false
     @State private var newScrumData = DailyScrum.Data()
+    let saveAction: ()->Void
     
     var body: some View {
         List{
@@ -46,10 +48,14 @@ struct ScrumsView: View {
                                 let newScrum = DailyScrum(data: newScrumData)
                                 scrums.append(newScrum)
                                 isPresentingNewScrumView = false
+                                newScrumData = DailyScrum.Data()
                             }
                         }
                     }
+                }
             }
+        .onChange(of: scenePhase)  { phase in
+            if phase == .inactive {saveAction()}
         }
     }
 }
@@ -57,7 +63,7 @@ struct ScrumsView: View {
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            ScrumsView(scrums: .constant(DailyScrum.sampleData))
+            ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
         }
     }
 }
