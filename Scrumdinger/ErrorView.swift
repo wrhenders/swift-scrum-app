accfd0dbbb44fc1797f6d54b1faf9333ebb24638
wrhenders <wrhenders@gmail.com>
2022-11-15 11:08:21 -0600

    Added Error handling for load and save

---
+++ Scrumdinger/ErrorView.swift
@@ -0,0 +1,52 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by Ryan Henderson on 11/15/22.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("An error has ocurred!")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWrapper.error.localizedDescription)
+                    .font(.headline)
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .cornerRadius(16)
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired, guidance: "You can safely ignore this error.")
+    }
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
